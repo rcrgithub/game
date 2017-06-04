250ab62b6acf899ce83ecf2278c4ee9ff8d220ce
Ricardo Capuz <rcr2012@gmail.com>
2017-06-03 20:55:11 -0400

    pone foto 855

--- app/controllers/wellcome_controller.rb
+++ app/controllers/wellcome_controller.rb
@@ -1,5 +1,9 @@
 class WellcomeController < ApplicationController
   def index
+    var1 = Profile.first
+    var2 = Profile.last
+    puts "var1   = #{var1.inspect}"
+    puts "var2   = #{var2.inspect}"
     @profiles = Profile.all
     puts  @profiles.inspect
     puts  @profiles.class
